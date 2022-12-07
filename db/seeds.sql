438363b740f85c7252b7df361e5436e4878f09dd
sanam1060 <sanam_g1990@yahoo.com>
2022-12-07 18:57:04 -0500

    finish setup db file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,20 @@
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO department (department_name)
 VALUES
-  ('Ronald', 'Firbank', 1, 1),
-  ('Virginia', 'Woolf', 1, 1);
+  ('HR'),
+  ('Tech'),
+  ('Marketing'),
+  ('Finance');
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-  ('Manager1 ', 2000, 1),
-  ('Secretary1', 3000, 2);
+  ('Recruiter ', 2000, 1),
+  ('Marketer', 3000, 2);
+  ('Software Engineer', 4000, 3),
+  ('CEO', 5000, 23);
 
-INSERT INTO department (name)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-  ('RH'),
-  ('Tech');
+  ('Ronald', 'Firbank', 1, 1),
+  ('Virginia', 'Woolf', 2, 2),
+  ('Unica', 'Zurn', 3, 1),
+  ('Octavia', 'Butler', 4, 3);
